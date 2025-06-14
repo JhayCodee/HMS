6ed2197fb0edef0776073fe8aab0e2ac9d877a84
JhayCodee <kluis0178@gmail.com>
2025-06-14 01:11:45 -0600

    chore(solution): reorganiza estructura en carpetas src/ y test/
    - Mueve los proyectos existentes bajo la carpeta src/
    - Crea la carpeta test/ para contener proyectos de pruebas
    - Añade proyecto MyHostel.Application.Tests con pruebas iniciales para validaciones usando FluentValidation y xUnit
    - Ejecuta correctamente dotnet test sin errores

---
+++ test/MyHostel.Application.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using FluentValidation.TestHelper;
+using MyHostel.Application.Seguridad.Usuario.Commands.CrearUsuario;
+
+public class CrearUsuarioCommandValidatorTests
+{
+    private readonly CrearUsuarioCommandValidator _validator = new();
+
+    [Fact]
+    public void Should_Have_Error_When_Email_Is_Invalid()
+    {
+        var command = new CrearUsuarioCommand
+        {
+            Nombre = "Prueba",
+            Email = "invalido",
+            Password = "123456"
+        };
+
+        var result = _validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(x => x.Email);
+    }
+}
